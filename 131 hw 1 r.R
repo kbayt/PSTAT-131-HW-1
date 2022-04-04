41712133ecd3ea74b94eec4a864cb17ee477c502
Katherine Bayt <kbayt@ucsb.edu>
2022-04-03 18:04:35 -0700

    Finishing hw 1

--- 131 hw 1 r.R
+++ 131 hw 1 r.R
@@ -2,30 +2,44 @@ library(tidymodels)
 library(tidyverse)
 library(ISLR)
 library(ggplot2)
+install.packages("corrplot")
+library(corrplot)
 ?mpg
 view(mpg)
 ?ggplot2
 
-#exercise 1
+#EXERCISE 1
+## histogram of hwy 
 hwy <- mpg$hwy
 hist(hwy)
 
-#exercise 2
-
+#EXERCISE 2
+## scatter plot with cyl and hwy
 cty <- mpg$cty
 plot(hwy, cty, main = "Relationship between City Miles per Gallon 
      and Highway Miles per Gallon",
      xlab = "Highway Miles per Gallon", ylab = "City Miles per Gallon")
 
-#exercise 3?
-counts = table(manufacturer)
-barplot(counts, main = "Miles per Gallon",
-        xlab = "Manufacturer")
-bp <- ggplot(data=mpg, mapping = aes(x=manufacturer, y=length())) + geom_col()
-bp
+#EXERCISE 3
+## bar plot of manufacturer
+bp <- ggplot(data=mpg, mapping = aes(x=manufacturer)) + geom_bar(stat = "count")
+bp 
 
-#exercise 4
-bp2 <- ggplot(data=mpg, mapping = aes(x=hwy, y=cyl)) + geom_col()
+## flipping manufacturers onto the y-axis 
+bp + coord_flip()
+## ordered bars by height 
+bp2 <- ggplot(data=mpg, mapping = aes(y=fct_infreq(manufacturer))) + geom_bar(stat = "count")
 bp2
+order(mpg$manufacturer)
 
-#exercise 5
+#EXERCISE 4
+## boxplot of hwy grouped by cyl 
+boxplot(mpg$hwy ~ mpg$cyl)
+
+#EXERCISE 5
+## removed non-numeric observations 
+newdata <- subset(mpg, select = -c(manufacturer,model, trans, class, fl, drv))
+## lower triangle correlation matrix 
+M = cor(newdata)
+corrplot(M, type = 'lower', diag = FALSE)
+
